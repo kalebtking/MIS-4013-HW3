ed239f6af3687ce4275fdc2035e14b79b97a2b28
kalebtking <kalebtking@icloud.com>
2024-10-28 22:46:07 -0500

    Update model-products.php

--- model-products.php
+++ model-products.php
@@ -1,18 +1,4 @@
-// <?php
-// function selectProducts() {
-//     try {
-//         $conn = get_db_connection();
-//         $stmt = $conn->prepare("use Homework3; select * from Products;");
-//         $stmt->execute();
-//         $result = $stmt->get_result();
-//         $conn->close();
-//         return $result;
-//     } catch (Exception $e) {
-//         $conn->close();
-//         throw $e;
-//     }
-// }
-// ?>
+
 <?php
 function selectProducts() {
     try {
