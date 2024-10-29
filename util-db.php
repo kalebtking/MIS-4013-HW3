38fb4024037c9d108d2fc9b24ddd0619207674f1
kalebtking <kalebtking@icloud.com>
2024-10-28 23:15:27 -0500

    Update util-db.php

--- util-db.php
+++ util-db.php
@@ -1,25 +1,20 @@
-// <?php
-// function get_db_connection(){
-//     // Create connection
-//     $conn = new mysqli("homework3db.mysql.database.azure.com", "kalebtking", "sooners!23", "homework3db");
-    
-//     // Check connection
-//     if ($conn->connect_error) {
-//       return false;
-//     }
-//     return $conn;
-// }
-// ?>
 <?php
-function get_db_connection(){
+function get_db_connection() {
+    $host = "homework3db.mysql.database.azure.com"; // Your Azure server hostname
+    $dbname = "homework3db"; // The name of your database as set in Azure
+    $username = "kalebtking"; // The username you use to log in
+    $password = "sooners!23"; // The password for the database
+
     try {
-        $conn = new PDO("mysql:host=homework3db.mysql.database.azure.com;dbname=homework3", "kalebtking", "sooners!23");
+        $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         return $conn;
     } catch (PDOException $e) {
+        echo "Connection failed: " . $e->getMessage();
         return false;
     }
 }
 ?>
+
 
 
