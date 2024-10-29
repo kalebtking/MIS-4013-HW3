8643470fdade6f3cb31524fdf190f1660f74083a
kalebtking <kalebtking@icloud.com>
2024-10-28 21:04:14 -0500

    Create view-products.php

---
+++ view-products.php
@@ -0,0 +1,27 @@
+<h1>Products</h1>
+<div class="table-responsive">
+  <table class="table">
+    <thead>
+      <tr>
+        <th>ID</th>
+        <th>Name</th>
+        <th>Price</th>
+        <th>Material</th>
+      </tr>
+    </thead>
+    <tbody>
+<?php
+while ($product = $products->fetch_assoc()) {
+?>
+  <tr> 
+    <td><?php echo $product["ProductID"]; ?></td>
+    <td><?php echo $product["ProductName"]; ?></td>
+    <td><?php echo $product["ProductPrice"]; ?></td>
+    <td><?php echo $product["ProductMaterial"]; ?></td>
+  </tr>
+<php 
+}
+?>
+    </tbody>
+  </table>
+</div>
