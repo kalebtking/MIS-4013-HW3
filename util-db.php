a3820523038c640819a550dc5f7507ac36dbd0d4
kalebtking <kalebtking@icloud.com>
2024-10-28 22:17:00 -0500

    Update util-db.php

--- util-db.php
+++ util-db.php
@@ -1,7 +1,7 @@
 <?php
 function get_db_connection(){
     // Create connection
-    $conn = new mysqli("homework3db.mysql.database.azure.com", "kalebtking", "sooners!23", "Homework3");
+    $conn = new mysqli("homework3db.mysql.database.azure.com", "kalebtking", "sooners!23", "homework3db");
     
     // Check connection
     if ($conn->connect_error) {
