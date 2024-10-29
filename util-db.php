a1dc8390c31e592ecfdcadbbf719af413c20f95d
kalebtking <kalebtking@icloud.com>
2024-10-28 22:38:44 -0500

    Update util-db.php

--- util-db.php
+++ util-db.php
@@ -1,13 +1,25 @@
+// <?php
+// function get_db_connection(){
+//     // Create connection
+//     $conn = new mysqli("homework3db.mysql.database.azure.com", "kalebtking", "sooners!23", "homework3db");
+    
+//     // Check connection
+//     if ($conn->connect_error) {
+//       return false;
+//     }
+//     return $conn;
+// }
+// ?>
 <?php
 function get_db_connection(){
-    // Create connection
-    $conn = new mysqli("homework3db.mysql.database.azure.com", "kalebtking", "sooners!23", "homework3db");
-    
-    // Check connection
-    if ($conn->connect_error) {
-      return false;
+    try {
+        $conn = new PDO("mysql:host=homework3db.mysql.database.azure.com;dbname=homework3db", "kalebtking", "sooners!23");
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        return $conn;
+    } catch (PDOException $e) {
+        return false;
     }
-    return $conn;
 }
 ?>
+
 
