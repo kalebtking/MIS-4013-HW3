5a380b19d2a4b6d62fb9516c8f2f7dd2feb506a9
kalebtking <kalebtking@icloud.com>
2024-10-28 22:43:57 -0500

    Update model-products.php

--- model-products.php
+++ model-products.php
@@ -1,15 +1,39 @@
+// <?php
+// function selectProducts() {
+//     try {
+//         $conn = get_db_connection();
+//         $stmt = $conn->prepare("use Homework3; select * from Products;");
+//         $stmt->execute();
+//         $result = $stmt->get_result();
+//         $conn->close();
+//         return $result;
+//     } catch (Exception $e) {
+//         $conn->close();
+//         throw $e;
+//     }
+// }
+// ?>
 <?php
 function selectProducts() {
     try {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("use Homework3; select * from Products;");
+
+        if (!$conn) {
+            throw new Exception("Failed to connect to the database.");
+        }
+
+        $conn->query("USE Homework3;");
+        $stmt = $conn->prepare("SELECT * FROM Products;");
         $stmt->execute();
         $result = $stmt->get_result();
         $conn->close();
         return $result;
     } catch (Exception $e) {
-        $conn->close();
+        if ($conn) {
+            $conn->close();
+        }
         throw $e;
     }
 }
 ?>
+
