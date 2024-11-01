3a0f5c8e5aa581a327e053177fb0f6be3f5d70ad
kalebtking <kalebtking@icloud.com>
2024-11-01 15:52:01 -0500

    Update util-db.php

--- util-db.php
+++ util-db.php
@@ -1,7 +1,13 @@
 <?php
 function get_db_connection(){
     // Create a mysqli connection
-    $conn = new mysqli("homework3db.mysql.database.azure.com", "kalebtking", "sooners!23", "homework3db");
+    $servername = "homework3db.mysql.database.azure.com";
+    $username = "kalebtking";
+    $password = "sooners!23";
+
+    // Create connection
+    $conn = mysqli_connect($servername, $username, $password);
+    // $conn = new mysqli("homework3db.mysql.database.azure.com", "kalebtking", "sooners!23", "homework3db");
 
     // Check connection
     if ($conn->connect_error) {
