b0e19b4e57197d5c0434fc53b033eb5639098406
kalebtking <kalebtking@icloud.com>
2024-11-04 15:43:06 -0600

    Update model-products.php

--- model-products.php
+++ model-products.php
@@ -1,55 +1,27 @@
-// <?php
-// require_once("util-db.php");
-
-// function selectProducts() {
-//     try {
-//         $conn = get_db_connection();
-
-//         if (!$conn) {
-//             throw new Exception("Failed to connect to the database.");
-//         }
-
-//         // Execute the query
-//         $query = "SELECT * FROM Products";
-//         $stmt = $conn->query($query);
-
-//         if (!$stmt) {
-//             throw new Exception("Query failed: " . $conn->error);
-//         }
-
-//         $result = $stmt->fetch_all(MYSQLI_ASSOC);
-//         $conn->close();
-//         return $result;
-//     } catch (Exception $e) {
-//         if ($conn) {
-//             $conn->close();
-//         }
-//         throw $e;
-//     }
-// }
-// ?>
 <?php
 require_once("util-db.php");
 
 function selectProducts() {
-    try {
-        $conn = get_db_connection();
-
-        if (!$conn) {
-            throw new Exception("Failed to connect to the database.");
-        }
-
-        // Execute the query
-        $query = "SELECT * FROM Products";
-        $stmt = $conn->query($query);
-        
-        $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
-        return $result;
-    } catch (Exception $e) {
-        throw $e;
+    $conn = get_db_connection();
+    
+    if (!$conn) {
+        throw new Exception("Failed to connect to the database.");
     }
+
+    // Execute the query
+    $query = "SELECT * FROM Products";
+    $result = mysqli_query($conn, $query);
+
+    if (!$result) {
+        throw new Exception("Query failed: " . mysqli_error($conn));
+    }
+
+    $products = mysqli_fetch_all($result, MYSQLI_ASSOC);
+    mysqli_close($conn); // Close the connection after the query
+    return $products;
 }
 ?>
+
 
 
 
