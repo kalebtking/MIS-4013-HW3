395ba767f6273e84d5a74beafd4e38192d499016
kalebtking <kalebtking@icloud.com>
2024-11-04 15:42:12 -0600

    Update util-db.php

--- util-db.php
+++ util-db.php
@@ -6,13 +6,18 @@ function get_db_connection()
     $password = 'sooners!23';
     $db_name = 'homework3db';
 
-    //Establishes the connection
+    // Establish the connection
     $conn = mysqli_init();
     mysqli_real_connect($conn, $host, $username, $password, $db_name, 3306);
+    
     if (mysqli_connect_errno($conn)) {
-    die('Failed to connect to MySQL: '.mysqli_connect_error());
-        {   
+        die('Failed to connect to MySQL: ' . mysqli_connect_error());
+    }
+    
+    return $conn; // Return the connection object on success
+}
 ?>
+
 
 // <?php
 // function get_db_connection(){
