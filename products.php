0ee1fa9496e362f64d9c2c1cb03f856aa86d6753
kalebtking <kalebtking@icloud.com>
2024-11-04 14:58:33 -0600

    Update products.php

--- products.php
+++ products.php
@@ -1,3 +1,15 @@
+// <?php
+// require_once("util-db.php");
+// require_once("model-products.php");
+
+// $pageTitle = "Products";
+// include "view-header.php";
+// include "view-footer.php";
+
+// // Get products from the database
+// $products = selectProducts();
+// include "view-products.php";
+// ?>
 <?php
 require_once("util-db.php");
 require_once("model-products.php");
@@ -10,4 +22,5 @@ include "view-footer.php";
 $products = selectProducts();
 include "view-products.php";
 ?>
+
 
