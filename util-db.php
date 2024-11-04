048020c38fe88835baa3bbe166609bb40c969df2
kalebtking <kalebtking@icloud.com>
2024-11-04 15:40:09 -0600

    Update util-db.php

--- util-db.php
+++ util-db.php
@@ -1,19 +1,35 @@
 <?php
-function get_db_connection(){
-    $dsn = "mysql:host=homework3db.mysql.database.azure.com;dbname=homework3db;charset=utf8mb4";
-    $username = "kalebtking";
-    $password = "sooners!23";
+function get_db_connection() 
+{
+    $host = 'homework3db.mysql.database.azure.com';
+    $username = 'kalebtking';
+    $password = 'sooners!23';
+    $db_name = 'homework3db';
 
-    try {
-        $conn = new PDO($dsn, $username, $password);
-        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        return $conn;
-    } catch (PDOException $e) {
-        // Output detailed error information for debugging
-        die("Database connection failed: " . $e->getMessage());
-    }
-}
+    //Establishes the connection
+    $conn = mysqli_init();
+    mysqli_real_connect($conn, $host, $username, $password, $db_name, 3306);
+    if (mysqli_connect_errno($conn)) {
+    die('Failed to connect to MySQL: '.mysqli_connect_error());
+        {   
 ?>
+
+// <?php
+// function get_db_connection(){
+//     $dsn = "mysql:host=homework3db.mysql.database.azure.com;dbname=homework3db;charset=utf8mb4";
+//     $username = "kalebtking";
+//     $password = "sooners!23";
+
+//     try {
+//         $conn = new PDO($dsn, $username, $password);
+//         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+//         return $conn;
+//     } catch (PDOException $e) {
+//         // Output detailed error information for debugging
+//         die("Database connection failed: " . $e->getMessage());
+//     }
+// }
+// ?>
 
 // <?php
 // function get_db_connection(){
