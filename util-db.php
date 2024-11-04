996cb468760501a9627884066318f52f359dd9bb
kalebtking <kalebtking@icloud.com>
2024-11-04 16:08:23 -0600

    Update util-db.php

--- util-db.php
+++ util-db.php
@@ -1,22 +1,33 @@
-<?php
-function get_db_connection() 
-{
-    $host = 'homework3db.mysql.database.azure.com';
-    $username = 'kalebtking';
-    $password = 'sooners!23';
-    $db_name = 'homework3db';
+// <?php
+// function get_db_connection() 
+// {
+//     $host = 'homework3db.mysql.database.azure.com';
+//     $username = 'kalebtking';
+//     $password = 'sooners!23';
+//     $db_name = 'homework3db';
 
-    // Establish the connection directly with mysqli_connect
-    $conn = mysqli_connect($host, $username, $password, $db_name, 3306);
+//     // Establish the connection directly with mysqli_connect
+//     $conn = mysqli_connect($host, $username, $password, $db_name, 3306);
 
-    if (!$conn) {
-        die('Failed to connect to MySQL: ' . mysqli_connect_error());
-    }
+//     if (!$conn) {
+//         die('Failed to connect to MySQL: ' . mysqli_connect_error());
+//     }
     
-    return $conn; // Return the connection object on success
+//     return $conn; // Return the connection object on success
+// }
+// ?>
+<?php
+function get_db_connection(){
+    // Create connection
+    $conn = new mysqli('homework3db.mysql.database.azure.com', 'kalebtking', 'sooners!23', 'homework3db');
+    
+    // Check connection
+    if ($conn->connect_error) {
+      return false;
+    }
+    return $conn;
 }
 ?>
-
 
 
 
