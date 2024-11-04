d4c3bf2145bc636cdc8c2e257f230e1d7ee82815
kalebtking <kalebtking@icloud.com>
2024-11-04 14:57:47 -0600

    Update model-products.php

--- model-products.php
+++ model-products.php
@@ -1,3 +1,33 @@
+// <?php
+// require_once("util-db.php");
+
+// function selectProducts() {
+//     try {
+//         $conn = get_db_connection();
+
+//         if (!$conn) {
+//             throw new Exception("Failed to connect to the database.");
+//         }
+
+//         // Execute the query
+//         $query = "SELECT * FROM Products";
+//         $stmt = $conn->query($query);
+
+//         if (!$stmt) {
+//             throw new Exception("Query failed: " . $conn->error);
+//         }
+
+//         $result = $stmt->fetch_all(MYSQLI_ASSOC);
+//         $conn->close();
+//         return $result;
+//     } catch (Exception $e) {
+//         if ($conn) {
+//             $conn->close();
+//         }
+//         throw $e;
+//     }
+// }
+// ?>
 <?php
 require_once("util-db.php");
 
@@ -12,22 +42,15 @@ function selectProducts() {
         // Execute the query
         $query = "SELECT * FROM Products";
         $stmt = $conn->query($query);
-
-        if (!$stmt) {
-            throw new Exception("Query failed: " . $conn->error);
-        }
-
-        $result = $stmt->fetch_all(MYSQLI_ASSOC);
-        $conn->close();
+        
+        $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
         return $result;
     } catch (Exception $e) {
-        if ($conn) {
-            $conn->close();
-        }
         throw $e;
     }
 }
 ?>
+
 
 
 
