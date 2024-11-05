fc45b71e050297920a6e7216dc9558c4477e9377
kalebtking <kalebtking@icloud.com>
2024-11-04 18:44:27 -0600

    Update model-products.php

--- model-products.php
+++ model-products.php
@@ -2,37 +2,17 @@
 require_once("util-db.php");
 
 function selectProducts() {
-    $conn = get_db_connection();
-
-    if (!$conn) {
-        throw new Exception("Failed to connect to the database.");
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("use Homework3; select * from Products;");
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
     }
-
-    // Execute the query
-    $query = "use homework3; SELECT * FROM Products";
-    $result = mysqli_query($conn, $query);
-
-    if (!$result) {
-        throw new Exception("Query failed: " . mysqli_error($conn));
-    }
-
-    // Fetch all rows as an associative array
-    $products = mysqli_fetch_all($result, MYSQLI_ASSOC);
-    
-    // Debug output
-    if (empty($products)) {
-        echo "Query executed, but no products found.";
-    } else {
-        echo "Products found: <pre>";
-        print_r($products);
-        echo "</pre>";
-    }
-
-    // Free result set and close connection
-    mysqli_free_result($result);
-    mysqli_close($conn);
-
-    return $products;
 }
 ?>
 
