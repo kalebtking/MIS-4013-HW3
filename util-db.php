63bb883585e233493340fc5fa61a09be6310ad43
kalebtking <kalebtking@icloud.com>
2024-11-07 20:04:40 -0600

    Update util-db.php

--- util-db.php
+++ util-db.php
@@ -1,12 +1,25 @@
 <?php
 function get_db_connection(){
-    // Create connection
-    $conn = new mysqli('homework3db.mysql.database.azure.com', 'kalebtking', 'sooners!23', 'homework3');
-    
+    // Create connection with SSL options
+    $conn = new mysqli(
+        'homework3db.mysql.database.azure.com', // Host
+        'kalebtking', // Username
+        'sooners!23', // Password
+        'homework3' // Database
+    );
+
+    // Set SSL options
+    $conn->ssl_set(NULL, NULL, '/Users/kalebking/Desktop/MIS-4013/DigiCertGlobalRootCA.crt.pem', NULL, NULL); // Adjust the path as needed
+
     // Check connection
     if ($conn->connect_error) {
-      return false;
+        return false;
     }
+
+    // Enforce SSL connection
+    $conn->options(MYSQLI_OPT_SSL_VERIFY_SERVER_CERT, true);
+
     return $conn;
 }
 ?>
+
