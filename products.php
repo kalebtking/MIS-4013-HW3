4dd618207b06e15132739f6090b28f2a2a539693
kalebtking <kalebtking@icloud.com>
2024-11-07 19:46:32 -0600

    Update products.php

--- products.php
+++ products.php
@@ -1,21 +1,18 @@
 <?php
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 require_once("util-db.php");
 require_once("model-products.php");
+
 
 $pageTitle = "Products";
 include "view-header.php";
 
 // Get products from the database
 $products = selectProducts();
-
-// Debug: Check if $products has data
-if (empty($products)) {
-    echo "No products found or error fetching products.";
-} else {
-    echo "<pre>";
-    print_r($products);
-    echo "</pre>";
-}
 
 include "view-products.php";
 include "view-footer.php";
