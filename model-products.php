3096c80a6770cd0c6b71c8bb1ab22437195798e1
kalebtking <kalebtking@icloud.com>
2024-11-07 19:38:25 -0600

    Update model-products.php

--- model-products.php
+++ model-products.php
@@ -2,17 +2,30 @@
 require_once("util-db.php");
 
 function selectProducts() {
-    try {
-        $conn = get_db_connection();
-        $stmt = $conn->prepare("use Homework3; select * from Products;");
-        $stmt->execute();
-        $result = $stmt->get_result();
-        $conn->close();
-        return $result;
-    } catch (Exception $e) {
-        $conn->close();
-        throw $e;
+    $conn = get_db_connection();
+
+    if ($conn) {
+        die("Database connection failed.");
     }
+
+    $sql = "select ProductID, ProductName, ProductPrice, ProductMaterial from Products"
+    $result = $conn->query($sql);
+
+    $products = [];
+    if ($result && $result->num_rows > 0) {
+        // Fetch data
+        while ($row = $result->fetch_assoc()) {
+            $products[] = $row;
+        }
+    }
+
+    if ($result) {
+        $result->close();
+    }
+
+    $conn->close();
+
+    return $products;
 }
 ?>
 
