21b76d4b46be2f2cda8c60285d1ef0cae975678f
kalebtking <kalebtking@icloud.com>
2024-11-09 17:32:10 -0600

    Update view-header.php

--- view-header.php
+++ view-header.php
@@ -6,6 +6,9 @@
     <title><?= $pageTitle ?> - RUN</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.1.3/css/bootstrap.min.css">
     <link rel="stylesheet" href="style.css"> <!-- Link to your external CSS file -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Rubik+Glitch&family=Work+Sans:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
 </head>
 <body>
     <nav class="navbar navbar-expand-lg navbar-light bg-light mb-4">
