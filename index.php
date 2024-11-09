789c1a2f8d08a9120ae68dc1f2e1de4e6b88a7aa
kalebtking <kalebtking@icloud.com>
2024-11-09 14:11:36 -0600

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,13 @@
 $pageTitle = "Home";
 include "view-header.php";
 ?>
-<h1>Homework 3</h1>
+<div class="mt-4">
+  <h1>Welcome to RUN</h1>
+  <p class="lead">Your go-to store for premium running shoes and apparel.</p>
+  <img src="images/hero-image.jpg" class="img-fluid rounded" alt="RUN store banner">
+  <p>Discover top-of-the-line products that enhance your running experience. Browse our <a href="products.php">Products</a> or learn more <a href="about.php">About Us</a>.</p>
+</div>
 <?php
 include "view-footer.php";
 ?>
+
