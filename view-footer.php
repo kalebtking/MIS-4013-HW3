3b5110f6f109c7086e1ca75ae9dab27260370893
kalebtking <kalebtking@icloud.com>
2024-11-09 14:12:16 -0600

    Update view-footer.php

--- view-footer.php
+++ view-footer.php
@@ -1,4 +1,9 @@
- </div>
+    </div>
+    <footer class="mt-4 text-center">
+      <p>&copy; <?= date("Y") ?> RUN. All Rights Reserved.</p>
+      <p><a href="privacy-policy.php">Privacy Policy</a> | <a href="contact.php">Contact Us</a></p>
+    </footer>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-</body>
+  </body>
 </html>
+
