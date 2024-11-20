6184d993e5d26d0de6c73e1a7c854998df2053a8
kalebtking <kalebtking@icloud.com>
2024-11-19 18:31:23 -0600

    Update util-db.php

--- util-db.php
+++ util-db.php
@@ -1,7 +1,7 @@
 <?php
 function get_db_connection(){
     // Create connection
-    $conn = new mysqli('138.197.17.168', 'kalebtki_hw3', 'sooners!23', 'kalebtki_hw3');
+    $conn = new mysqli('138.197.17.168', 'kalebtki_user', 'sooners!23', 'kalebtki_hw3');
     
     // Check connection
     if ($conn->connect_error) {
